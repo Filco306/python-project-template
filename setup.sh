07970478db64a33edabaa296dad0782cf82fe7aa
Filip Cornell <filipcornell@Filips-MacBook-Pro-2.local>
2020-06-25 21:00:24 +0200

    added some stuff

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ source venv/bin/activate
 python3 -m pre-commit | grep -q 'No module named' && pip3 install pre-commit || echo 'pre-commit is installed'
 pip3 install -r requirements.txt
 mv prep-folder/pre-push .git/hooks/pre-push
+rm README.md
+mv prep-folder/Readme-template.md README.md
 rm -rf prep-folder
