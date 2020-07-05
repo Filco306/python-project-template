5edb061238c48966d4dcfd48a8e0d44bbc3a7e11
Filip Cornell <filipcornell@Filips-MacBook-Pro-2.local>
2020-07-05 13:24:13 +0200

    Added chmod to ensure that pre-push file is executable

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ python3 -m pre-commit | grep -q 'No module named' && pip3 install pre-commit || 
 python3 -m pytest | grep -q 'No module named' && pip3 install pytest || echo 'pre-commit is installed'
 pip3 install -r requirements.txt
 mv setup-folder/pre-push .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
 rm README.md
 mv setup-folder/Readme-template.md README.md
 rm -rf setup-folder
